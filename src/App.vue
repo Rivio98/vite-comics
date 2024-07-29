8e240d2efddaa1ca83dbde110b18af2636b236c7
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-29 16:23:44 +0200

    [UPD] Add components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script >
-
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default {
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
 </script>
 
 <template>
-  
+ <AppHeader />
+ <AppMain />
+<AppFooter />
 </template>
 
 <style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 <template >
     <div>
-        
+        ciao
     </div>
 </template>
 <style lang="scss" scoped >
