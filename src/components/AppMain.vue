423dfbd2ff40691d003e4c55347b5e96ca9dd381
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-30 17:49:52 +0200

    [UPD] Added labels and for, exercise finished

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,7 +11,9 @@ export default {
 </script>
 <template>
     <div class="bg-cartoon">
-
+        <span class="badge text-bg-primary rounded-0 px-4 py-2">
+            <h3>CURRENT SERIES</h3>
+        </span>
     </div>
     <div>
         <div class="wrap bg-dark">
@@ -19,12 +21,15 @@ export default {
                 <div class="row">
                     <div class="col-12 p-5 d-flex flex-wrap justify-content-center">
                         <div v-for="(comic, index) in comics" :key="index" class="col-6 col-md-4 col-lg-2 mb-4 d-flex">
-                            <div class="card bg-transparent w-100 mx-3">
+                            <div class="card border-0 bg-transparent w-100 mx-3">
                                 <img :src="comic.thumb" :alt="comic.series" class="card-img-top img-fluid">
                                 <div>
-                                    <h5 class="text-white text-center">{{ comic.series }}</h5>
+                                    <h5 class="text-white text-center pt-2">{{ comic.series }}</h5>
                                 </div>
                             </div>
+                        </div>
+                        <div>
+                            <button class="btn btn-primary rounded-0 px-5">LOAD MORE</button>
                         </div>
                     </div>
                 </div>
@@ -73,12 +78,19 @@ export default {
 
 .bg-cartoon {
     background-image: url('../assets/img/jumbotron.jpg');
-    height: 450px;
+    height: 550px;
     background-size: cover;
+    position: relative;
+
+    .badge {
+        position: absolute;
+        bottom: -30px;
+        left: 550px
+    }
 }
 
 .card {
-    border: none;
+
 
     img {
         width: 200px;
