1b0a348f44935a620847321424fae2901edb2a83
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-30 15:48:12 +0200

    [UPD] Added  new background and json file with array

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,80 +1,88 @@
 <script>
 export default {
-    
+
 }
 </script>
-<template >
+<template>
+    <div class="bg-cartoon">
+
+    </div>
     <div>
         <div class="wrap bg-dark">
-         <div class="container">
-            <div class="row">
-                <div class="col-12 p-5">
-                <p class="text-light">Content goes here</p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 p-5">
+                        <p class="text-light">Content goes here</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="wrap bg-lightblue p-5">
+            <div class="container">
+                <div class="row text-light p-0 m-0 row-col-5">
+                    <div class="col">
+                        <div class="content text-center">
+                            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                            <p class="d-inline-block">DIGITAL COMICS</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="content text-center">
+                            <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                            <p class="d-inline-block">DC MERCHANDISE</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="content text-center">
+                            <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                            <p class="d-inline-block">SUBSCRIPTION</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="content text-center">
+                            <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                            <p class="d-inline-block">COMIC SHOP LOCATOR</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="content text-center">
+                            <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                            <p class="d-inline-block">DC POWER VISA</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="wrap bg-lightblue p-5">
-        <div class="container">
-            <div class="row text-light p-0 m-0 row-col-5">
-                <div class="col">
-                    <div class="content text-center">
-                        <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                        <p class="d-inline-block">DIGITAL COMICS</p>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="content text-center">
-                        <img src="../assets/img/buy-comics-merchandise.png" alt="">
-                        <p class="d-inline-block">DC MERCHANDISE</p>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="content text-center">
-                        <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-                        <p class="d-inline-block">SUBSCRIPTION</p>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="content text-center">
-                        <img src="../assets/img/buy-comics-shop-locator.png" alt="">
-                        <p class="d-inline-block">COMIC SHOP LOCATOR</p>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="content text-center">
-                        <img src="../assets/img/buy-dc-power-visa.svg" alt="">  
-                        <p class="d-inline-block">DC POWER VISA</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-     </div>
-  </div> 
 </template>
 <style lang="scss">
-    @use '../styles/partials/variables';
+@use '../styles/partials/variables';
 
+.bg-cartoon {
+    background-image: url('../assets/img/jumbotron.jpg');
+    height: 450px;
+    background-size: cover;
+}
 
-.bg-lightblue{
-        background-color: variables.$primary_color;
+.bg-lightblue {
+    background-color: variables.$primary_color;
 
-     .col{
+    .col {
         padding: 0px;
         margin: 0px;
         align-content: center;
         align-self: center;
 
 
-        p{
+        p {
             padding-left: 0px;
         }
 
         img {
-        max-width: 70px;
-        max-height: 70px;
-        margin-right: 5px;
+            max-width: 70px;
+            max-height: 70px;
+            margin-right: 5px;
+        }
     }
-    }
-    }
+}
 </style>
